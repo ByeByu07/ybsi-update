87e0c3e4a0e048a378d5dffa9b72f265e515842a
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2026-01-22 22:27:32 +0700

    feat: setup organization plugin better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,10 @@ import { db } from '@/db'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from '@/db/schema'
+import { organization } from "better-auth/plugins"
 
 export const auth = betterAuth({
+  plugins: [ organization() ],
   database: drizzleAdapter(db, {
         provider: "pg",
         schema: schema
